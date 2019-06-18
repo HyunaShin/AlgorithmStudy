6a62fcc25b100aa70777a27a1d9cc9225891654e
HyunaShin <hyuna1615@naver.com>
2019-06-18 15:53:12 +0900

    added description

--- coding_interview_conquer/dfs.py
+++ coding_interview_conquer/dfs.py
@@ -12,6 +12,8 @@ def dfs(graph, start):
     3. pop으로 stack에서 노드를 꺼낸다
     4. stack에 3번에서 pop한 노드의 인접 노드를 넣는다.(이 때 이미 방문한 노드는 무시한다.)
     5. 이미 pop한 노드를 visited(방문한 노드의 리스트)에 넣는다.
+    이를 응용하면 그래프 내의 사이클 찾기를 할 수 있다.
+    visited에 중복된 노드가 있다 --> 그래프 내의 사이클이 존재한다.
     '''
     vertex_list, edge_list = graph
     visited_vertex = []
